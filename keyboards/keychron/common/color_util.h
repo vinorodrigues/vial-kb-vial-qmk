e0762073d2d32b41859e2e3a0e629933cd649080
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-02-09 13:02:53 +1100

    Preparation for git submodules of common

--- keyboards/keychron/common/color_util.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Vino Rodrigues (@vinorodrigues)
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-#pragma once
-#include "color.h"
-
-RGB to_rgb(uint8_t r, uint8_t g, uint8_t b);
-HSV to_hsv(uint8_t h, uint8_t s, uint8_t v);
-HSV rgb_to_hsv(RGB rgb);
