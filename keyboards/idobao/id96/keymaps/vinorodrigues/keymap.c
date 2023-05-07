f7b9114b2197ee669a7e896d3fe756b8d0de467d
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-05-07 15:33:16 +1000

    clean up of id96 code

--- keyboards/idobao/id96/keymaps/vinorodrigues/keymap.c
+++ keyboards/idobao/id96/keymaps/vinorodrigues/keymap.c
@@ -4,11 +4,13 @@
 #include QMK_KEYBOARD_H
 #include "idobao_common.h"
 
+#define KC_APPLE_FN_KEY (KC_VERSION + 1)
+#define KC_APPL KC_APPLE_FN_KEY
 #define KC_NUMLK KC_NUM_LOCK
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_SNAP, KC_SIRI, KC_LOKD,  KC_MUTE, KC_VOLD, KC_VOLU,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_SNAP, KC_SIRI, KC_LOCK,  KC_MUTE, KC_VOLD, KC_VOLU,
         KC_GRV,  KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC, TG(1)  ,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,          KC_HOME,  KC_UP,   KC_PGUP, XXXXXXX,
         KC_CAPS, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,                    KC_LEFT,  KC_APPL, KC_RGHT, KC_PPLS,
@@ -68,9 +70,17 @@ void housekeeping_task_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_record_idobao(keycode, record)) {
-        return false;
+    if (!process_record_idobao(keycode, record)) { return false; }
+
+    #ifdef APPLE_FN_ENABLE
+    switch (keycode) {
+        case MAGIC_TOGGLE_NKRO:
+        case KC_APPLE_FN_KEY:   return idobao_register_code(record, KC_APPLE_FN); break;
+        case MAGIC_HOST_NKRO:
+        case MAGIC_UNHOST_NKRO: return false; break;  // discard these keys
+        default: break;
     }
+    #endif  // APPLE_FN_ENABLE
 
     return true;  /* Process all other keycodes normally */
 }
